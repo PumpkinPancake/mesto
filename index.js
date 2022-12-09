52e5486f8b182841f1c9339a57fb3aa517183b52
Mauri <Quri.mau@gmail.com>
2022-12-09 15:47:48 +0300

    develop mest

---
+++ index.js
@@ -0,0 +1,34 @@
+let popup = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__button-closed');
+let addButton = document.querySelector('.profile__edit-button');
+let title = document.querySelector('.profile__title');
+let about = document.querySelector('.profile__subtitle');
+let submit = document.querySelector('.popup__submit');
+
+
+popup.classList.remove('popup_opened');
+
+addButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+function addName() {
+    let enterName = document.querySelector('.input__popup-name');
+    let enterAbout = document.querySelector('.input__popup-about');
+
+    title.innerHTML = `<h1 class="profile__title">${enterName.value}</h1>`;
+    about.innerHTML = `<h2 class="profile__subtitle">${enterAbout.value}</h2>`;
+    closePopup();
+
+    enterName.value = '';
+    enterAbout.value = '';
+}
+
+    submit.addEventListener('click', addName);
